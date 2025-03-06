d1746aa423bc6f794371467c1cfe3f15721c3a92
milo33 <65894437+milo1150@users.noreply.github.com>
2025-03-06 19:17:48 +0700

    add: NATS token

--- internal/nats/nats_client.go
+++ internal/nats/nats_client.go
@@ -9,9 +9,10 @@ import (
 
 // Connects to NATS_URL: nats://nats:4222 (Docker service name).
 func ConnectNATS() *nats.Conn {
-	NATSUrl := os.Getenv("NATS_URL")
+	url := os.Getenv("NATS_URL")
+	token := os.Getenv("NATS_TOKEN")
 
-	nc, err := nats.Connect(NATSUrl)
+	nc, err := nats.Connect(url, nats.Token(token))
 	if err != nil {
 		log.Fatalf("Failed to connect NATS server")
 	}
