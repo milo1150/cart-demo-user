75b15d071065dc9751833ca95f814d0fa59708f4
milo33 <65894437+milo1150@users.noreply.github.com>
2025-06-11 01:14:01 +0700

    fix: use origin value from env

--- internal/middlewares/cors.go
+++ internal/middlewares/cors.go
@@ -1,14 +1,18 @@
 package middlewares
 
 import (
+	"os"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 func CORS() echo.MiddlewareFunc {
+	origin := os.Getenv("ORIGIN_URL")
+
 	return middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{
-			"http://localhost:3000",
+			origin,
 		},
 		AllowMethods: []string{
 			echo.GET,
